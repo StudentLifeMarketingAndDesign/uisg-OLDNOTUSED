2742a8f62f80253e7366b7260c55e7c4d4175949
fidibidi <fidel.nagy@gmail.com>
2013-11-14 12:07:23 -0600

    admin

--- mysite/_config.php
+++ mysite/_config.php
@@ -39,4 +39,4 @@ RecaptchaField::$public_api_key = '6LcjsAgAAAAAAD6MXE7QNLusIBMajgpfK_EWjL3C';
 RecaptchaField::$private_api_key = '6LcjsAgAAAAAAJNAXp7BiEvEas6scIeWzQdaS87c';
 //enable fulltext search
 FulltextSearchable::enable(); 
-Security::setDefaultAdmin("admin","password");
+//Security::setDefaultAdmin("admin","password");
